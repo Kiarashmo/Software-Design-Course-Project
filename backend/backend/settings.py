3b873cf85e776e146a0a9a4186788ea063440bd6
Amir-Ali <a2motaghedy@gmail.com>
2022-12-06 01:47:50 +0330

    added basic react project and connected it to django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '../frontend/build'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    '../frontend/build/static'
+]
