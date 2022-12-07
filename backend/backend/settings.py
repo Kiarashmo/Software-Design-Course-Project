a6c3922320c5b6320e0d67a0b1251115bc460c5c
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2022-12-07 11:46:55 +0330

    unix windows conflict on line 60 setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_BASE_DIR, 'frontend/build')
+            os.path.join(BASE_BASE_DIR, 'frontend', 'build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
