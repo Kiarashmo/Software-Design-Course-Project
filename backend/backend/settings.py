c370c6811756e12290d4924872ed56ae3ccfad66
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2022-12-07 10:58:29 +0330

    fixed the unix windows conflict

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_BASE_DIR, 'frontend/build/static')
+     os.path.join(BASE_BASE_DIR, 'frontend', 'build', 'static')
     
 ]
