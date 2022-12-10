09ef6e4a778db4ceb27a2f2f5ec8175d4248bb0d
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2022-12-10 16:37:53 +0330

    Login and Register
    added login and register components but they need a  lot of work.

---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+function Login({ Login, error }) {
+    const [details, setDetails] = useState({name: "", email: "", password: ""});
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        Login(details);
+    }
+  return (
+    <form onSubmit={submitHandler}>
+        <div className='form-inner'>
+            <h2>Login</h2>
+            {/* ERROR!  */}
+            <div className="form-group">
+                <label htmlFor='email'>Email: </label>
+                <input type="email" name="email" id="email" />
+            </div>
+            <div className="form-group">
+                <label htmlFor='password'>Password: </label>
+                <input type="password" name="password" id='password'/>
+            </div>
+            <input type="submit" value="LOGIN" />
+        </div>
+    </form>
+  )
+}
+
+export default Login
