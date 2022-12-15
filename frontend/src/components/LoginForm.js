4cd61c52bef660701b861919d6a5ea7df5a1de7f
Kiarash Mokhtari <mokhtarikiarash@gmial.com>
2022-12-16 02:09:43 +0330

    completed login form js and css files.

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import "./LoginForm.css"
 
 function Login({ Login, error }) {
     const [details, setDetails] = useState({name: "", email: "", password: ""});
@@ -11,17 +12,27 @@ function Login({ Login, error }) {
   return (
     <form onSubmit={submitHandler}>
         <div className='form-inner'>
-            <h2>Login</h2>
+            <h1 id='login-head'>HELLO AGAIN!</h1>
+            <h2>welcome back you've been missed</h2>
             {/* ERROR!  */}
+
             <div className="form-group">
-                <label htmlFor='email'>Email: </label>
-                <input type="email" placeholder="Enter the username" name="email" id="email" />
+                <input type="email" placeholder="Enter the email" name="email" id="email" />
             </div>
+
             <div className="form-group">
-                <label htmlFor='password'>Password: </label>
-                <input type="password" name="password" id='password'/>
+                <input type="password" placeholder="Enter the password" name="password" id='password'/>
             </div>
-            <input type="submit" value="Login" />
+
+            <div className='signin-btn'>Sign in</div>
+
+            <h3 class="hr-lines">OR</h3>
+
+            <div className='alt-login'>
+                <div className='google'></div>
+                <div className='linkedin'></div>
+            </div>
+
         </div>
     </form>
   )
