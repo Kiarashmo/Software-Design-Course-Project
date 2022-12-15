743bf76cfb81c503373b32a11a2bf75c1c53f7ae
MehradEsmaeilZad81 <mehrad.es81@gmail.com>
2022-12-15 14:11:21 +0330

    Created Tag model

--- backend/main/models.py
+++ backend/main/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Tag(models.Model):
     title = models.CharField(max_length=255)
     courses = models.ManyToManyField('Course')
+
     def __str__(self):
         return self.title
 
