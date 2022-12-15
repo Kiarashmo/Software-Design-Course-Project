a418a2b079328efe168634acd48d2aa38de3ff4a
MehradEsmaeilZad81 <mehrad.es81@gmail.com>
2022-12-15 11:25:47 +0330

    Created Member model

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+@admin.register(models.Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Member(models.Model):
+    MEMBERSHIP_SIMPLE = 'S'
+    MEMBERSHIP_MENTOR = 'M'
+
+    MEMBERSHIP_CHOICES = (
+        (MEMBERSHIP_SIMPLE, 'Simple'),
+        (MEMBERSHIP_MENTOR, 'Mentor')
+    )
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=14)  # Example : (98) 911 111 1111
+    join_date = models.DateTimeField(auto_now=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_SIMPLE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
