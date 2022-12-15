d31f25f56901de08be76d6f2eff6f11ef131e6fe
MehradEsmaeilZad81 <mehrad.es81@gmail.com>
2022-12-15 14:51:38 +0330

    Fixed some problems

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -9,11 +9,11 @@ from . import models
 
 @admin.register(models.Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
-    list_editable = ['membership']
+    list_display = ('first_name', 'last_name', 'membership')
+    list_editable = ('membership',)
     list_per_page = 10
-    ordering = ['first_name', 'last_name']
-    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+    ordering = ('first_name', 'last_name')
+    search_fields = ('first_name__istartswith', 'last_name__istartswith')
 
 
 @admin.register(models.Course)
@@ -21,10 +21,10 @@ class CourseAdmin(admin.ModelAdmin):
     def get_tags(self, obj):
         return "\n".join([t.title for t in obj.tags.all()])
 
-    list_display = ['title', 'create_date', 'get_tags']
+    list_display = ('title', 'create_date', 'get_tags')
     list_per_page = 10
-    ordering = ['title', 'create_date']
-    search_fields = ['title', 'get_tags']
+    ordering = ('title', 'create_date')
+    search_fields = ('title', 'get_tags')
 
 
 @admin.register(models.Tag)
@@ -32,7 +32,7 @@ class TagAdmin(admin.ModelAdmin):
     def course_count(self, obj):
         return obj.course_set.all().count()
 
-    list_display = ['title', 'course_count']
+    list_display = ('title', 'course_count')
     list_per_page = 10
-    ordering = ['title']
-    search_fields = ['title', 'course_count']
+    ordering = ('title', 'course_count')
+    search_fields = ('title', 'course_count')
