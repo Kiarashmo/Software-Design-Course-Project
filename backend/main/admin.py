fe56765f5c0dd9f6db982e9a2baca84e28e6699c
MehradEsmaeilZad81 <mehrad.es81@gmail.com>
2022-12-17 12:44:31 +0330

    Created User profile

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -11,8 +11,9 @@ from . import models
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'membership')
     list_editable = ('membership',)
+    list_select_related = ('user',)
     list_per_page = 10
-    ordering = ('first_name', 'last_name')
+    ordering = ('user__first_name', 'user__last_name')
     search_fields = ('first_name__istartswith', 'last_name__istartswith')
 
 
