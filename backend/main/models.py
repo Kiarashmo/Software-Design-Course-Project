811c9a714ab6544795426347ecb4f511c5889ee3
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2022-12-18 16:51:35 +0330

    Fixed some problems After the problems arose

--- backend/main/models.py
+++ backend/main/models.py
@@ -28,20 +28,33 @@ class Course(models.Model):
 
 
 class Member(models.Model):
-    MEMBERSHIP_SIMPLE = 'S'
-    MEMBERSHIP_MENTOR = 'M'
-
-    MEMBERSHIP_CHOICES = (
-        (MEMBERSHIP_SIMPLE, 'Simple'),
-        (MEMBERSHIP_MENTOR, 'Mentor')
-    )
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=14)  # Example : (98) 911 111 1111
     birth_date = models.DateField(null=True, blank=True)
     join_date = models.DateTimeField(auto_created=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
+
+
+class Mentor(models.Model):
+    phone = models.CharField(max_length=14)  # Example : (98) 911 111 1111
+    birth_date = models.DateField(null=True, blank=True)
+    create_date = models.DateTimeField(auto_created=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
