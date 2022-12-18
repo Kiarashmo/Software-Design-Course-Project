1779b57e19263851204643ab9b3c0a2286d6503f
MehradEsmaeilZad81 <mehrad.es81@gmail.com>
2022-12-18 12:42:05 +0330

    Edited models and made it better

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -9,11 +9,19 @@ from . import models
 
 @admin.register(models.Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'membership')
-    list_editable = ('membership',)
+    list_display = ('first_name', 'last_name',)
     list_select_related = ('user',)
     list_per_page = 10
-    ordering = ('user__first_name', 'user__last_name')
+    ordering = ('user__first_name', 'user__last_name', 'join_date')
+    search_fields = ('first_name__istartswith', 'last_name__istartswith')
+
+
+@admin.register(models.Mentor)
+class MentorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    list_select_related = ('user',)
+    list_per_page = 10
+    ordering = ('user__first_name', 'user__last_name', 'create_date')
     search_fields = ('first_name__istartswith', 'last_name__istartswith')
 
 
