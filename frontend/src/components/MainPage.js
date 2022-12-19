ed1304c12ae1454a72cfc2f6de896ca9a95db389
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2022-12-19 17:39:55 +0330

    improving css of  the main page

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return(
         <main-page>
             <div className="icon-text">
-                <div className="main-tex">
+                <div className="main-text">
                     <h1>Welcome to cohort!</h1>
                     <p>Find like-minded people.<br/>
                         learn together.</p>
