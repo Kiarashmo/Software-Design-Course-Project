630aef52f1155c05e373bb3fcf208c1cde3493df
Kiarash Mokhtari <mokhtarikiarash@gmail.com>
2022-12-20 01:48:34 +0330

    Added alt-register div

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,32 +1,58 @@
-import React from 'react'
-import "./RegisterForm.css"
+import React from "react";
+import "./RegisterForm.css";
 
 export default function RegisterForm() {
   return (
     <div>
       <form>
-        <div className='form-inner'>
-            <h2>Register</h2>
-            {/* ERROR!  */}
-            <div className='form-group'>
-                <label htmlFor='name'>Name: </label>
-                <input type="text" name="name" id="name" />
+        <div className="register-form-inner">
+          <h1 className="register-h1">Register</h1>
+          {/* ERROR!  */}
+          <div className="register-form-group">
+            <input
+              className="register-form-input"
+              type="text"
+              name="username"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="register-form-group">
+            <input
+              className="register-form-input"
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="register-form-group">
+            <input
+              className="register-form-input"
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="register-form-group">
+            <input
+              className="register-form-input"
+              type="re-password"
+              name="re-password"
+              placeholder="Reenter your password"
+            />
+          </div>
+          <div className="register-form-group">
+            <input className="register-btn" type="submit" value="Register" />
+          </div>
+          <div className="register-form-divider">
+            <h3 className="register-hr-lines register-h3">OR</h3>
+            <div className="alt-register">
+              <button className="google"></button>
+              <button className="linkedin"></button>
             </div>
-            <div className="form-group">
-                <label htmlFor='email'>Email: </label>
-                <input type="email" name="email" id="email" />
-            </div>
-            <div className="form-group">
-                <label htmlFor='password'>Password: </label>
-                <input type="password" name="password" id='password'/>
-            </div>
-            <div className="form-group">
-                <label htmlFor='re-password'>Re-Enter Password: </label>
-                <input type="re-password" name="re-password" id='re-password'/>
-            </div>
-            <input type="submit" value="Register" />
+          </div>
+          <div className="back-to-login"><h4 className="register-h4">Already have an account? <a>Sign in</a></h4></div>
         </div>
       </form>
     </div>
-  )
+  );
 }
