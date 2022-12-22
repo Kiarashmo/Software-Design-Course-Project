b48e596b86580fe10344869bc635f5684f783287
Kiarash Mokhtari <mokhtarikiarash@gmail.com>
2022-12-22 23:12:49 +0330

    fixed navbar and added signin-btn to it

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Navbar.css";
+
 export default function Navbar() {
-    return(
-        <nav>
-            <h3 className="nav-btn">Cohort</h3>
-            <h4>Home</h4>
-            <h4>Discover</h4>
-            <h4>Join</h4>
-
-        </nav>
-    )
-
+  return (
+    <nav>
+      <h3 className="nav-logo">Cohort</h3>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Discover</li>
+        <li>Join</li>
+      </ul>
+      <ul className="nav-links">
+        <button className="nav-singin-btn">Sign in</button>
+      </ul>
+    </nav>
+  );
 }
