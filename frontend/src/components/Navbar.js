0747348ccce6361a4490db9812c7c93ef4ad3b64
Amir-Ali Motaghedy <70889826+geekn0rd@users.noreply.github.com>
2022-12-28 02:06:02 +0330

    fixed nav style with Link now

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,25 +5,25 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/" className="nav-link-header">
-        <h3 className="nav-logo">Cohort</h3>
-      </Link>
-      <ul className="nav-links">
-        <Link to="/" style={{ textDecoration: 'none' }} className="nav-links--li">
-          <li className="nav-links--li">Home</li>
+      <div className="left-part">
+        <Link to="/">
+          <h3 className="nav-logo">Cohort</h3>
         </Link>
-        <Link to="/courses" style={{ textDecoration: 'none' }} className="nav-links--li">
-          <li className="nav-links--li">Discover</li>
+        <Link to="/" style={{ textDecoration: 'none' }} >
+          <h3 className="nav-item">Home</h3>
         </Link>
-        <Link to="/register" style={{ textDecoration: 'none' }} className="nav-links--li">
-          <li className="nav-links--li">Join</li>
-        </Link>
-      </ul>
-      <ul className="nav-links">
-        <Link to="/log-in" style={{ textDecoration: 'none', background: 'none' }}>
-          <button className="nav-singin-btn">Sign in</button>
-        </Link>
-      </ul>
+          <Link to="/courses" style={{ textDecoration: 'none' }} >
+              <h3 className="nav-item">Discover</h3>
+          </Link>
+            <Link to="/register" style={{ textDecoration: 'none' }}>
+              <h3 className="nav-item">Join</h3>
+            </Link>
+        </div>
+        <div>
+          <Link to="/log-in" style={{ textDecoration: 'none', background: 'none' }}>
+            <h3 className="sign-in-btn">Sign in</h3>
+          </Link>
+         </div>
     </nav>
   );
 }
